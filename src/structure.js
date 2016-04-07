7f0786bddaec870b174e204562df4a38863b2690
Miguel Pignatelli <emepyc@gmail.com>
2016-04-07 13:54:50 +0100

    flat2tree adapted to new associations structure

--- src/structure.js
+++ src/structure.js
@@ -2,7 +2,6 @@
 
 // Given a flat structure of associations
 // convert and return that structure to a tree
-// TODO: Should this be moved to the cttvApi package?
 var rootId = "cttv_root";
 var flat2tree = function (data) {
     var associations = data.data;
@@ -19,7 +18,9 @@ var flat2tree = function (data) {
         "name" : "cttv_disease",
         "disease": {
             "id": rootId,
-            "name": "cttv_disease"
+            "efo_info": {
+                "label": ""
+            }
         }
     };
     var index = {};
@@ -30,6 +31,8 @@ var flat2tree = function (data) {
     for (var i=0; i<sortedAssociations.length; i++) {
         addNode(sortedAssociations[i], tree, tas, index);
     }
+    console.log("TREE...");
+    console.log(tree);
     return tree;
 };
 
@@ -37,15 +40,15 @@ function unfoldAssoc (arr) {
     var unfold = [];
     for (var i=0; i<arr.length; i++) {
         var node = arr[i];
-        if (node.disease.path.length > 1) {
-            for (var j=1; j<node.disease.path.length; j++) {
+        if (node.disease.efo_info.path.length > 1) {
+            for (var j=1; j<node.disease.efo_info.path.length; j++) {
                 var clone = cloneNode(node);
-                clone.currPath = node.disease.path[j];
+                clone.currPath = node.disease.efo_info.path[j];
                 unfold.push(clone);
             }
-            node.currPath = node.disease.path[0];
+            node.currPath = node.disease.efo_info.path[0];
         } else {
-            node.currPath = node.disease.path[0];
+            node.currPath = node.disease.efo_info.path[0];
         }
         // We are getting the root here for now, so filter out the root (ie, path is empty)
         if (!node.currPath) {
@@ -60,8 +63,10 @@ function unfoldAssoc (arr) {
 function addNode (node, tree, tas, index) {
     // nodes don't have a name, we put one there
     node.__id = node.disease.id;
-    node.__name = node.disease.name;
-    node.name = node.disease.name;
+    node.__name = node.disease.efo_info.label;
+    node.name = node.disease.efo_info.label;
+    node.__association_score = node.association_score.overall;
+    node.__evidence_count = node.evidence_count.total;
     // for (var i=0; i<node.disease.path.length; i++) {
         //var path = node.disease.path[i];
         var path = node.currPath;
@@ -70,20 +75,23 @@ function addNode (node, tree, tas, index) {
         delete(node.currPath);
 
         // If the parent is cttv_root and the node is not a therapeutic area (path.length > 2) we search for the TA in the set of TAs
-        if ((parent.__id === rootId) && (path.length > 2)) {
-            var taId = node.target.id + "-" + path[1];
+        if ((parent.__id === rootId) && (path.length > 1)) {
+            var taId = node.target.id + "-" + path[0];
             var ta = tas[taId];
-            if (!ta) {
-                ta = {};
-                ta.__name = ta.disease.name;
-                ta.name = ta.disease.name;
-                ta.disease = {};
-                ta.disease.id = path[1];
-                ta.disease.name = path[1];
+            if (!ta) { // This shouldn't happen;
+                console.error("We do not have TA: " + taId);
+                // ta = {};
+                // ta.__name = ta.disease.efo_info.label; // BROKEN!
+                // ta.name = ta.disease.efo_info.label;
+                // ta.disease = {};
+                // ta.disease.id = path[1];
+                // ta.disease.efo_info.label = path[1];
             }
-            ta.__id = path[1];
-            ta.__name = ta.disease.name;
-            ta.name = ta.disease.name;
+            ta.__id = path[0];
+            ta.__name = ta.disease.efo_info.label;
+            ta.name = ta.disease.efo_info.label;
+            ta.__association_score = ta.association_score.overall;
+            ta.__evidence_count = ta.evidence_count.total;
             if (!tree.children) {
                 tree.children = [];
             }
@@ -139,6 +147,7 @@ function findParent (path, index) {
             return index[path[i]];
         }
     }
+    return index.cttv_root;
 }
 
 // function findNode (nodeId, tree) {
@@ -171,31 +180,59 @@ function sortByPathLength (a, b) {
 // Fast clone methods
 function cloneNode (n) {
     var c = {
-        association_score: n.association_score,
-        evidence_count: n.evidence_count,
+        __association_score: n.__association_score,
+        __evidence_count: n.__evidence_count,
         id: n.id,
         __id: n.__id,
         __name: n.__name,
         target: {
             id: n.target.id,
-            name: n.target.name,
-            symbol: n.target.symbol
+            name: n.target.gene_info.name,
+            symbol: n.target.gene_info.symbol
         },
-        disease: {
-            id: n.disease.id,
-            name: n.disease.name,
-        },
-        datatypes: cloneDatatypes(n.datatypes)
+        disease: cloneDisease(n.disease),
+        // disease: {
+        //     id: n.disease.id,
+        //     name: n.disease.efo_info.label,
+        // },
+        association_score: cloneAssociationScore(n.association_score),
+        evidence_count: cloneEvidenceCount(n.evidence_count)
     };
     if (n.is_direct) {
         c.is_direct = n.is_direct;
     }
-    var paths = [];
-    for (var i=0; i<n.disease.path.length; i++) {
-        paths.push(clonePath(n.disease.path[i]));
-    }
-    c.disease.path = paths;
+    // var paths = [];
+    // for (var i=0; i<n.disease.efo_info.path.length; i++) {
+    //     paths.push(clonePath(n.disease.efo_info.path[i]));
+    // }
+    // c.disease.path = paths;
     return c;
+}
+
+function cloneDisease (disease) {
+    var d = {
+        id: disease.id,
+        efo_info: cloneEfoInfo(disease.efo_info)
+    };
+
+    return d;
+}
+
+function cloneEfoInfo (efo) {
+    var e = {
+        label: efo.label,
+        path: clonePath(efo.path),
+        therapeutic_area: cloneTherapeuticArea(efo.therapeutic_area)
+    };
+    return e;
+}
+
+function cloneTherapeuticArea (tas) {
+    var ta = {
+        codes: cloneArr(tas.codes),
+        labels: cloneArr(tas.labels)
+    };
+    return ta;
 }
 
 function clonePath (p) {
@@ -206,38 +243,87 @@ function clonePath (p) {
     return c;
 }
 
-function cloneDatatypes (dts) {
-    var cs = [];
-    for (var i=0; i<dts.length; i++) {
-        cs.push(cloneDatatype(dts[i]));
-    }
-    return cs;
+function cloneEvidenceCount (ect) {
+    var ec = {
+        total: ect.total,
+        datatype: cloneObj(ect.datatype),
+        datasource: cloneObj(ect.datasource)
+    };
+    return ec;
 }
 
-function cloneDatatype (d) {
-    var c = {
-        association_score: d.association_score,
-        datatype: d.datatype,
-        evidence_count: d.evidence_count
+function cloneAssociationScore (asr) {
+    var as = {
+        overall: asr.overall,
+        datatypes: cloneObj(asr.datatypes),
+        datasources: cloneObj(asr.datasources)
     };
 
-    // Datasources
-    var dsrcs = [];
-    for (var i=0; i<d.datasources.length; i++) {
-        dsrcs.push(cloneDatasource[d.datasources[i]]);
-    }
-    c.datasources = dsrcs;
-    return c;
+    return as;
 }
 
-function cloneDatasource (d) {
-    var c = {
-        association_score: d.association_score,
-        datasource: d.datasource,
-        evidence_count: d.evidence_count
-    };
-    return c;
+function cloneArr (arr) {
+    var a = [];
+    for (var i=0; i<arr.length; i++) {
+        a.push(arr[i]);
+    }
+    return a;
 }
+
+function cloneObj (obj) {
+    var o = {};
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            o[key] = obj[key];
+        }
+    }
+    return o;
+}
+
+// function cloneDatasources (dsr) {
+//     var ds = {};
+//     for (var key in dsr) {
+//         if (dsr.hasOwnProperty(key)) {
+//             ds[key] = dsr[key];
+//         }
+//     }
+//     return ds;
+// }
+//
+// function cloneDatatypes (dts) {
+//     var cs = {};
+//     for (var key in dts) {
+//         if (dts.hasOwnProperty(key)) {
+//             cs[key] = dts[key];
+//         }
+//     }
+//     return cs;
+// }
+
+// function cloneDatatype (d) {
+//     var c = {
+//         association_score: d.association_score,
+//         datatype: d.datatype,
+//         evidence_count: d.evidence_count
+//     };
+//
+//     // Datasources
+//     var dsrcs = [];
+//     for (var i=0; i<d.datasources.length; i++) {
+//         dsrcs.push(cloneDatasource[d.datasources[i]]);
+//     }
+//     c.datasources = dsrcs;
+//     return c;
+// }
+//
+// function cloneDatasource (d) {
+//     var c = {
+//         association_score: d.association_score,
+//         datasource: d.datasource,
+//         evidence_count: d.evidence_count
+//     };
+//     return c;
+// }
 
 module.exports = exports = {
     flat2tree: flat2tree
